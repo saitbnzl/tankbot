3d5ba53e3308ec0886cca5bb46fa35b4cc3eb48c
saitbnzl <saitbnzl@gmail.com>
2025-12-02 13:35:36 +0300

    hailo

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@
 # After sourcing, your PYTHONPATH will be set for the current terminal session.
 
 # Define virtual environment name
-VENV_NAME="venv"
+VENV_NAME="cv"
 
 # Function to check if the script is being sourced
 is_sourced() {
