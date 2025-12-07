68e65cf1098a5855c692b679d5773b00a99c8a19
saitbnzl <saitbnzl@gmail.com>
2025-12-07 18:33:37 +0300

    rotate

--- tankbot_brain_server.py
+++ tankbot_brain_server.py
@@ -99,7 +99,7 @@ def frame_grabber():
         ok, frame = cap.read()
         if ok and frame is not None:
             # Rotate if your ESP32-CAM orientation requires it
-            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            #frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
             update_latest_frame(frame)
         else:
             print("[FRAME][WARN] Frame grab failed, forcing reconnect", flush=True)
